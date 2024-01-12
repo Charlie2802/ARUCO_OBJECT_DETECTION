e55b14d3cb5972ba38572a42e4a572147a55c0c2
Charlie2802 <aaditya.darakh@gmail.com>
2024-01-12 20:14:23 +0530

    obj detection best midel 500 epochs

--- config4.py
+++ config4.py
@@ -1,8 +1,8 @@
 import os
 from ultralytics import YOLO
 
-ROOT_DIR = '/Users/aaditya/Desktop/aruco-markers-2-4'
+ROOT_DIR = '/home/user/Desktop/ARUCO_OBJECT_DETECTION'
 
 model = YOLO("yolov8n.yaml") 
 
-results = model.train(data=os.path.join(ROOT_DIR, "aruco4.yaml"), epochs=1) 
+results = model.train(data=os.path.join(ROOT_DIR, "aruco4.yaml"), epochs=500) 
